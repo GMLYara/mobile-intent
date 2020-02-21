91b46c9b3b1e180ee8447e1ff39802019b377ffa
FARHAN <farhan.araya87@gmail.com>
2020-02-21 09:52:23 +0700

    INtent

--- app/src/main/java/id/ac/polinema/intent/MainActivity.java
+++ app/src/main/java/id/ac/polinema/intent/MainActivity.java
@@ -16,9 +16,25 @@ public class MainActivity extends AppCompatActivity {
 
     public void handleExplicitIntent(View view) {
         Intent intent = new Intent(this, ExplicitIntentActivity.class);
+        startActivity(intent);
     }
 
     public void handleExit(View view) {
         finish();
+    }
+
+    public void handleImplicitIntent(View view) {
+        Intent intent = new Intent(this, ImplicitIntentActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleAndroidBundle(View view) {
+        Intent intent = new Intent(this, BundleActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleAndroidParcelable(View view) {
+        Intent intent = new Intent(this, ParcelableActivity.class);
+        startActivity(intent);
     }
 }
